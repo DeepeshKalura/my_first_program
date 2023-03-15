cb6e65c69714f5b186a5c2843c146a0603684073
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-15 13:05:12 +0530

    I just print hello world

--- bin/my_first_program.dart
+++ bin/my_first_program.dart
@@ -1,5 +1,3 @@
-import 'package:my_first_program/my_first_program.dart' as my_first_program;
-
-void main(List<String> arguments) {
-  print('Hello world: ${my_first_program.calculate()}!');
+void main() {
+  print("hello world");
 }
